a2f425733408214e473e788c26c313a16f071b9f
oddhorse <15389659+oddhorse@users.noreply.github.com>
2026-04-07 19:54:39 -0400

    #loll

--- funny.js
+++ funny.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const Masto = require('masto')
 
-const TWEET_INTERVAL_MIN = 1 / 60
+const TWEET_INTERVAL_MIN = 30
 const TWEET_INTERVAL_MS = 1000 * 60 * TWEET_INTERVAL_MIN
 
 const m = Masto.createRestAPIClient({
@@ -55,7 +55,7 @@ const laughSprinkle = (min = 0, max = 4) => {
 	const rand = getRandInt(min, max)
 	let sprinkle = ''
 
-	if (rollChance(1 / 2))
+	if (rollChance(2, 3))
 		for (let i = 0; i < rand; i++) {
 			sprinkle += "😂"
 			if (rollChance(1, 4)) sprinkle += ' ' // stick extra space in there sometimes
@@ -65,7 +65,7 @@ const laughSprinkle = (min = 0, max = 4) => {
 
 const prefix = () => {
 	if (rollChance(2, 3)) return ""
-	const opts = ["", "LOL!", "this one is FUNNY ", "humor", "lolll", "hahaaa", "i'm dyinggg"]
+	const opts = ["LOL!", "this one is FUNNY ", "humor", "lolll", "hahaaa", "i'm dyinggg"]
 	const rand = getRandInt(0, opts.length - 1)
 	const final = `${opts[rand]} `
 	return final
@@ -73,14 +73,71 @@ const prefix = () => {
 
 const exacerbator = () => {
 	if (rollChance(1, 2)) return ""
-	const opts = ["", "LOL!", "loll...", "lolll", "hahaaa", "omgg", "💀"]
+	const opts = ["LOL!", "loll...", "lolll", "hahaaa", "omgg", "💀"]
 	const rand = getRandInt(0, opts.length - 1)
 	const final = `${opts[rand]} `
 	return final
 }
 
+const hashtag = () => {
+	if (rollChance(1, 4)) return ""
+	const opts = ["LOL", "lol", "funny", "humor", "jokes", "haha"]
+	const rand = getRandInt(0, opts.length - 1)
+	const final = `#${opts[rand]} `
+	return final
+}
+
 const writeReply = (origMsg) => {
-	const replyMsg = `${prefix()}${laughSprinkle()} they said ${origMsg} ${exacerbator()}${laughSprinkle(1, 4)}`
+	let pref = prefix()
+	let spr1 = laughSprinkle()
+	let qMark = ''
+	let exac = exacerbator()
+	let spr2 = laughSprinkle(1, 4)
+
+	// inject laugh if it's otherwise blank
+	if (pref.trim() == "" &&
+		spr1.trim() == "" &&
+		exac.trim() == "" &&
+		spr2.trim() == "") {
+		pref = "😂😂"
+	}
+
+	// randomize order of clauses
+	let p1, p2, p3, p4
+	if (rollChance(1, 2)) {
+		p1 = pref
+		p2 = spr1
+	} else {
+		p1 = spr1
+		p2 = pref
+	}
+	if (rollChance(1, 2)) {
+		p3 = exac
+		p4 = spr2
+	} else {
+		p3 = spr2
+		p4 = exac
+	}
+
+	/*
+	let ht1 = '', ht2 = '', ht3 = '', ht4 = ''
+	const roll = getRandInt(1, 4)
+	if (roll === 1) ht1 = hashtag()
+	if (roll === 2) ht2 = hashtag()
+	if (roll === 3) ht3 = hashtag()
+	if (roll === 4) ht4 = hashtag()
+	*/
+
+	let ht1 = hashtag()
+	let ht2 = hashtag()
+	let ht3 = hashtag()
+	let ht4 = hashtag()
+
+	// wrap original tweet in quotes if short enough, or random
+	if (origMsg.length <= 7 || rollChance(1, 3)) qMark = '"'
+
+	let replyMsg = `${p1}${ht1}${p2}${ht2} they said ${qMark}${origMsg}${qMark} ${p3}${ht3}${p4}${ht4}`
+	replyMsg = replyMsg.trim()
 	console.log(`reply: ${replyMsg}`)
 
 	return replyMsg
@@ -131,5 +188,6 @@ const pushSlop = async () => {
 pushSlop()
 
 setInterval(() => {
-	writeReply("testing")
+	//writeReply("testing")
+	pushSlop()
 }, TWEET_INTERVAL_MS)
