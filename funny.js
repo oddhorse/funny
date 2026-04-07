df94ee3402bd3e1aedaa91c48d189470bf8a9075
oddhorse <15389659+oddhorse@users.noreply.github.com>
2026-04-07 18:54:44 -0400

    reply geenrator

--- funny.js
+++ funny.js
@@ -1,6 +1,9 @@
 require('dotenv').config()
 const Masto = require('masto')
 
+const TWEET_INTERVAL_MIN = 1 / 60
+const TWEET_INTERVAL_MS = 1000 * 60 * TWEET_INTERVAL_MIN
+
 const m = Masto.createRestAPIClient({
 	url: 'https://networked-media.itp.io',
 	accessToken: process.env.TOKEN
@@ -25,6 +28,13 @@ const getRandInt = (min, max) => {
 	return rand
 }
 
+
+const rollChance = (part, whole) => {
+	const roll = getRandInt(1, whole)
+	if (roll <= part) return true
+	return false
+}
+
 // UNUSED
 const makeStatus = async () => {
 	let rand = Math.floor(Math.random() * 6)
@@ -41,6 +51,41 @@ const makeStatus = async () => {
 	console.log(`status link: ${s.url}`)
 }
 
+const laughSprinkle = (min = 0, max = 4) => {
+	const rand = getRandInt(min, max)
+	let sprinkle = ''
+
+	if (rollChance(1 / 2))
+		for (let i = 0; i < rand; i++) {
+			sprinkle += "😂"
+			if (rollChance(1, 4)) sprinkle += ' ' // stick extra space in there sometimes
+		}
+	return sprinkle
+}
+
+const prefix = () => {
+	if (rollChance(2, 3)) return ""
+	const opts = ["", "LOL!", "this one is FUNNY ", "humor", "lolll", "hahaaa", "i'm dyinggg"]
+	const rand = getRandInt(0, opts.length - 1)
+	const final = `${opts[rand]} `
+	return final
+}
+
+const exacerbator = () => {
+	if (rollChance(1, 2)) return ""
+	const opts = ["", "LOL!", "loll...", "lolll", "hahaaa", "omgg", "💀"]
+	const rand = getRandInt(0, opts.length - 1)
+	const final = `${opts[rand]} `
+	return final
+}
+
+const writeReply = (origMsg) => {
+	const replyMsg = `${prefix()}${laughSprinkle()} they said ${origMsg} ${exacerbator()}${laughSprinkle(1, 4)}`
+	console.log(`reply: ${replyMsg}`)
+
+	return replyMsg
+}
+
 
 const getTimeline = async (limit = 5) => {
 	const result = await m.v1.timelines.public.list({
@@ -49,6 +94,11 @@ const getTimeline = async (limit = 5) => {
 	return result
 }
 
+/**
+ * removes <p> tags wrapping input text
+ * @param {string} text - original text to scrub
+ * @returns scrubbed, <p>-free text
+ */
 const scrubPs = (text) => {
 	return text.substring(3, text.length - 4)
 }
@@ -63,7 +113,7 @@ const pushSlop = async () => {
 
 	// TODO: 2. author a reply to the post
 	const origMsg = scrubPs(target.content)
-	const replyMsg = `they said ${origMsg} 😂😂😂`
+	const replyMsg = writeReply(origMsg)
 
 	// TODO: 3. validate reply to post against server rules
 
@@ -80,5 +130,6 @@ const pushSlop = async () => {
 
 pushSlop()
 
-
-//setInterval(makeStatus, 10000)
+setInterval(() => {
+	writeReply("testing")
+}, TWEET_INTERVAL_MS)
